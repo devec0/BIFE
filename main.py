4f5a856270d8bb7de30891fa34b1af53a3d22d70
devec0 <163363163+devec0@users.noreply.github.com>
2024-12-13 17:30:29 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
-print("This is still WIP. Type 'bIfE' below to proceed.(case sensivite)")
+import os
+
+print("This is still WIP. Type 'proceed' below to proceed.")
 choice = input("Type: ")
-if choice == "bIfE":
-	print("Welcome to BIFE! (Built In File Editor)")
-	print("Read files directly from Python!")
-	type = input("What would you like to do? (write, read, new) : ").lower()
-	file = input("What file? (Needs to be in this folder and needs to include the file format. If using 'new' parameter this doesn't apply eg: work.txt: ")
-		
-	if type == "write":
-		writetype = input("Append or Overwrite? : ").lower()
-		if writetype == "overwrite":
-			opf = open(file, "w")
-			writeparam = input("File content : ")
-			opf.write(writeparam)
-			opf.close()
-			
-			opf = open(file, "r")
-			print(opf.read())
-			
-		if writetype == "append":
-			opf = open(file, "a")
-			writeparam = input("File content : ")
-			opf.write(writeparam)
-			opf.close
-			
-			opf = open(file, "r")
-			print(opf.read())
-			
-	if type == "read":
-		opf = open(file, "r")
-		print(opf.read())
-		opf.close
-	
-	if type == "new":
-		print("DISCLAIMER: You can't set the file path yet.")
-		opf = open(file, "x")
+
+if choice == "proceed":
+    print("Welcome to STE! (Simple Text Editor)")
+    print("Read files directly from Python!")
+
+    while True:
+        type = input("What would you like to do? (write, read, new, delete, dir): ").lower()
+        file = input("What file? (Needs to be in this folder and needs to include the file format, e.g., work.txt): ")
+        
+        if type == "write":
+            writetype = input("Append or Overwrite? : ").lower()
+            writeparam = input("File content: ")
+            if writetype == "overwrite":
+                opf = open(file, "w")
+                opf.write(writeparam)
+                opf.close()
+
+                opf = open(file, "r")
+                print(opf.read())
+                opf.close()
+
+            if writetype == "append":
+                opf = open(file, "a")
+                opf.write(writeparam)
+                opf.close()
+
+                opf = open(file, "r")
+                print(opf.read())
+                opf.close()
+
+        if type == "read":
+            opf = open(file, "r")
+            print(opf.read())
+            opf.close()
+
+        if type == "new":
+            print("DISCLAIMER: You can't set the file path yet.")
+            opf = open(file, "x")
+         
+        if type == "delete":
+         	if file != main.py:
+         		os.remove(file)
+         	
+         	else:
+         		print("You cannot remove BIFE.")
+         	
